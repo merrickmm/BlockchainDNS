c8c53d962a5d9b656d0d4fb068174b7ed0a13c1f
merrickmm <55674351+merrickmm@users.noreply.github.com>
2020-02-27 12:27:01 -0500

    block sock

--- testSock.py
+++ testSock.py
@@ -6,6 +6,7 @@ pushBlock = dnsChain()
 pushBlock.addBlock("192.168.1.1")
 pushBlock.addBlock("192.168.1.2")
 
+space = " "
             
 def testPush():    
     HOST = '127.0.0.1'  # The server's hostname or IP address
@@ -15,9 +16,22 @@ def testPush():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         for i in range(0, length):
-            test = str(pushBlock.blocks[i].index)
-            print (test)
-            s.send(test.encode('utf-8'))
+            index = str(pushBlock.blocks[i].index)
+            timestamp = str(pushBlock.blocks[i].timestamp)
+            ip = str(pushBlock.blocks[i].ip)
+            pHash = str(pushBlock.blocks[i].pHash)
+            cHash = str(pushBlock.blocks[i].cHash)
+
+            s.send(index.encode('utf-8'))
+            s.send(space.encode('utf-8'))
+            s.send(timestamp.encode('utf-8'))
+            s.send(space.encode('utf-8'))
+            s.send(ip.encode('utf-8'))
+            s.send(space.encode('utf-8'))
+            s.send(pHash.encode('utf-8'))
+            s.send(space.encode('utf-8'))
+            s.send(cHash.encode('utf-8'))
+
             data = s.recv(1024)
             time.sleep(2)
 
